6215a128da28153660655a94ed56c11821a59107
hafeezrq <hafeezrq@yahoo.com>
2022-12-18 22:04:20 +1100

    Pick random element in array and display.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import AnimalShow from './AnimalShow';
 
+function getRandomAnimal() {
+  const myAnimals = ['Bird', 'Cow', 'Gator', 'Horse', 'Cat', 'Dog'];
+  return myAnimals[Math.floor(Math.random() * myAnimals.length)];
+}
+
 const App = () => {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
   return (
     <div>
-      <AnimalShow />
+      <button onClick={handleClick}>Add Animal</button>
+      <p>Number of Animals = {animals}</p>
     </div>
   );
 };
